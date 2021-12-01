b13eca9fbe66910a460765b5107ff47d2f058096
VVestin <weston.metzler@gmail.com>
2021-11-30 22:47:16 -0700

    Refactor 2021 day 1 (WAY simpler solution)

--- 2021/day01.js
+++ 2021/day01.js
@@ -1,15 +1,7 @@
-let input = require('./base')(1)
+const input = require('./base')(1)
    .split('\n').map(Number)
 let count = 0
-let windowSum = 0
-for (let i = 0; i < input.length; i++) {
-   let newSum = windowSum + input[i]
-   if (i >= 3) {
-      newSum -= input[i - 3]
-      if (newSum > windowSum)
-         count++
-      //console.log(i, input[i], windowSum, newSum, newSum > windowSum)
-   }
-   windowSum = newSum
-}
+for (let i = 3; i < input.length; i++)
+   if (input[i] > input[i - 3])
+      count++
 console.log(count)
