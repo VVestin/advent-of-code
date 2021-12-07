45fca35bdadc1188a6b42a174e719f6983f7ec4a
VVestin <weston.metzler@gmail.com>
2021-12-06 22:11:13 -0700

    2021 day 6

---
+++ 2021/day06.js
@@ -0,0 +1,12 @@
+let fish = require('./base')(6).split(',')
+
+fishByDay = new Array(9).fill(0)
+for (let d of fish) fishByDay[d]++
+
+for (let i = 0; i < 256; i++) {
+   const newFish = fishByDay[0]
+   for (let day = 0; day < 8; day++) fishByDay[day] = fishByDay[day + 1]
+   fishByDay[6] += newFish
+   fishByDay[8] = newFish
+}
+console.log(fishByDay.reduce((a, b) => a + b, 0))
